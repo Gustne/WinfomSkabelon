b0ef09b1be2ce3fad7967f49cbf10750e0cbff19
Gustne <148748819+Gustne@users.noreply.github.com>
2024-02-27 17:20:24 +0100

    Add project files.

---
+++ DataAccess/PersonDA.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Text;
+using Models;
+
+namespace DataAccess
+{
+    public class PersonDA
+    {
+        string connString;
+
+        public PersonDA()
+        {
+            connString = ConfigurationManager.ConnectionStrings["BojeSQL"].ConnectionString;
+        }
+
+        public async Task<List<Person>> GetAsync()
+        {
+            string command = "SELECT * FROM Person";
+            using SqlConnection dbConn = new SqlConnection(connString);
+            SqlCommand sqlCommand = new SqlCommand(command, dbConn);
+
+            return await LoadData(sqlCommand, dbConn);
+        }
+
+        public async Task<Person> GetAsync(int id)
+        {
+            string command = "SELECT * FROM Kunder where Id = @Id";
+            using SqlConnection dbConn = new SqlConnection(connString);
+            SqlCommand sqlCommand = new SqlCommand(command, dbConn);
+            sqlCommand.Parameters.AddWithValue("@Id", id);
+            List<Person> output = await LoadData(sqlCommand, dbConn);
+
+            if (output.Count == 0)
+            {
+                return new Person();
+            }
+            else
+            {
+                return output[0];
+            }
+
+        }
+
+        public bool Create(Person person)
+        {
+            using SqlConnection dbConn = new SqlConnection(connString);
+            ConnectToSql(dbConn);
+
+            string command = "INSERT INTO Kunder(ErSælger, ErKøber, Fnavn, Lnavn, CPRnr, Tlf, Mail)" +
+                            " values(@Sælger, @Køber, @Fornavn, @Efternavn, @CPRnr, @Tlf, @Mail)";
+            using SqlCommand sqlCommand = new SqlCommand(command, dbConn);
+            AddParameters(sqlCommand, kunde);
+
+            return ExecuteSql(sqlCommand, dbConn);
+
+        }
+        private Task<bool> ConnectToSql(SqlConnection dbConn)
+        {
+            try
+            {
+                dbConn.Open();
+            }
+            catch (Exception)
+            {
+                if (dbConn.State == System.Data.ConnectionState.Open)
+                {
+                    dbConn.Close();
+                }
+                return false;
+            }
+            return true;
+        }
+
+        private async Task<List<Person>> LoadData(SqlCommand sqlCommand, SqlConnection dbConn)
+        {
+            List<Person> persons = new List<Person>();
+            try
+            {
+                await dbConn.OpenAsync();
+            }
+            catch (Exception)
+            {
+                // her skal vi måske få den til at kaste en exception som vi kan håndtere i UI når den ikke får forbindelse
+                if (dbConn.State == System.Data.ConnectionState.Open)
+                {
+                    await dbConn.CloseAsync();
+                }
+                return persons;
+            }
+
+            try
+            {
+                using SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    Person person = new Person();
+                    person.Id = (int)reader["Id"];
+                    person.FirstName = (string)reader["FirstName"];
+                    person.LastName = (string)reader["LastName"];
+                    person.Address = (string)reader["Address"];
+                    person.City = (string)reader["City"];
+                    person.PostalCode = (int)reader["PostalCode"];
+                    person.Email = (string)reader["Email"];
+                    person.Phone = (string)reader["Phone"];
+
+                    persons.Add(person);
+
+                }
+
+            }
+            catch (Exception)
+            {
+                //igen noget errorhandling
+            }
+            finally
+            {
+                await dbConn.CloseAsync();
+            }
+            return persons;
+        }
+
+    }
+
+}
---
+++ Models/Validering.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Models
+{
+    public class Validering
+    {
+
+        public bool isMatch(string input, string pattern)
+        {
+            Regex regex = new Regex(pattern);
+            bool isValid = regex.IsMatch(input);
+            if (isValid)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isMailValid(string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                string regexStatement = @"^[a-zA-Z0-9æøåÆØÅ]+@[a-zA-Z0-9.-æøåÆØÅ]+\.[a-zA-Z]{2,}$";
+                if (isMatch(input, regexStatement))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool isPhoneValid(string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                string regexStatement = @"^\d{8}$";
+                if (isMatch(input, regexStatement))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool isNameValid(string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                string regexStatement = @"^[a-zA-ZæøåÆØÅ]{2,}$";
+                if (isMatch(input, regexStatement))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool isAddressValid(string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                string regexStatement = @"^(?=.*[a-zA-ZæøåÆØÅ])(?=.*\d)[a-zA-ZæøåÆØÅ\d\s]+$";
+                if (isMatch(input, regexStatement))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
